3198a4a02267efa3ce0d0ea19abf8f616e432128
Bern67 <h2o.bern@gmail.com>
2015-09-25 22:40:51 -0700

    Waypoint filter and save as CSV file

--- Xs.R
+++ Xs.R
@@ -1,8 +1,16 @@
-####Bern Romey, merge DataPlus dbf files and save as CSV.
+#### Bern Romey, 24Sept15 
+#### Purpose: Merge DataPlus dbf files and save as CSV, export GPS waypoints as CSV
 
-require(foreign)# Install foreign package
+require(foreign)# load foreign package
+library(dplyr)# Load dplyr package
 hdr <-read.dbf("R39SiteInfo.dbf", as.is = FALSE)
 morph <- read.dbf("R39Morph.dbf", as.is = FALSE)
-dta <- merge(hdr, morph)
+dta <- merge(hdr, morph) #Merge hdr and morph dbf files based on common key
+str(dta) # check structure of data
+
 write.csv(dta,file = "R39Xs.csv") # Writes file with merged data to working directory
+
+wp <- filter(dta, ILAT > 0 )
+write.csv(wp,file = "IP39XsWP.CSV") # Writes CSV file with waypoints only to working directory
+
 
